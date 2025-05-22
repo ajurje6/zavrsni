655ff48bfd6b8f42df80e40fc53002214da03fed
ajurje6 <antola.lukic@gmail.com>
2025-05-22 09:33:01 +0200

    removed weather dashboard

--- frontend/app/Sodar/page.tsx
+++ frontend/app/Sodar/page.tsx
@@ -1,5 +1,4 @@
 import Navbar from "../components/Navbar/Navbar";
-import WeatherDashboard from "../components/WeatherDashboard/WeaherDashboard";
 import WindSummaryTable from "../components/WindSummaryTable/WindSummaryTable";
 import SodarPlot from "../components/SodarPlot/SodarPlot";
 import WindSpeedGraph from "../components/WindSpeedGraph/WindSpeedGraph";
@@ -13,7 +12,6 @@ export default function Sodar(){
             <WindSpeedGraph/>
             <WindDirectionGraph/>
             <WindVectorPlot/>
-            <WeatherDashboard/>
             <WindSummaryTable/>
         </main>
     );
